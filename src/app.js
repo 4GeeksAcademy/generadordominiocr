c332eaf8bad30b23470a52c2d94b16b49811ec47
cristobalorellanav <87153747+cristobalorellanav@users.noreply.github.com>
2025-04-28 15:21:36 +0000

    entregadodominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let pronombre = ['el', 'mi', 'yo'];
+let adjetivo = ['hermoso', 'intolerable'];
+let sustantivo = ['juego', 'tele'];
+let extension = ['.com', '.cl', '.py', '.arg'];
+
+let get_html = document.getElementById('listaDominios');
+
+window.onload = function Dominios() {
+    for (let a = 0; a < pronombre.length; a++) {
+        for (let b = 0; b < adjetivo.length; b++) {
+            for (let c = 0; c < sustantivo.length; c++) {
+                for (let d = 0; d < extension.length; d++) {
+                    let domain = pronombre[a] + adjetivo[b] + sustantivo[c] + extension[d];
+
+                    
+                    let domainListItem = document.createElement('li');
+                    domainListItem.textContent = domain;
+                  
+                    get_html.appendChild(domainListItem);
+
+                    console.log(domain); 
+                }
+            }
+        }
+    }
+}
+
+
+Dominios();
